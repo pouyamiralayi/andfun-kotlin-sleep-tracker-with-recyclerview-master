5cc4c40d64fa65d718e93bafbd620e796e9cf6e1
sarpedon333 <sarpedon000@gmail.com>
2019-10-17 23:10:50 +0330

    binding adapters

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/CustomerAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/CustomerAdapter.kt
@@ -2,72 +2,62 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.ListAdapter
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.Customer
+import com.example.android.trackmysleepquality.databinding.CustomerItemViewBinding
 
-class CustomerAdapter : ListAdapter<Customer,CustomerAdapter.ViewHolder>(CustomerDiffCallback()) {
+class CustomerAdapter : ListAdapter<Customer, CustomerAdapter.ViewHolder>(CustomerDiffCallback()) {
 
 
-
-    override fun onBindViewHolder(holder: CustomerAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
         holder.bind(item)
     }
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomerAdapter.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent)
     }
 
-    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val nameView: TextView = itemView.findViewById(R.id.nameView)
-        val noView: TextView = itemView.findViewById(R.id.noView)
-        val finView: TextView = itemView.findViewById(R.id.finView)
-        val owedView: TextView = itemView.findViewById(R.id.owedView)
-        val ownedView: TextView = itemView.findViewById(R.id.ownedView)
-        val descView: TextView = itemView.findViewById(R.id.descView)
+    class ViewHolder private constructor(val binding: CustomerItemViewBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: Customer) {
-            finView.text = item.finYear
-            nameView.text = item.customerName
-            noView.text = item.customerNo
-            descView.text = item.description
-            owedView.text = item.owed
-            ownedView.text = item.owned
+            binding.finView.text = item.finYear
+            binding.nameView.text = item.customerName
+            binding.noView.text = item.customerNo
+            binding.descView.text = item.description
+            binding.owedView.text = item.owed
+            binding.ownedView.text = item.owned
 
             if (!item.owed.equals("0")) {
-                owedView.setTextColor(Color.RED)
+                binding.owedView.setTextColor(Color.RED)
             } else {
-                owedView.setTextColor(Color.GRAY)
+                binding.owedView.setTextColor(Color.GRAY)
             }
 
             if (!item.owned.equals("0")) {
-                ownedView.setTextColor(Color.GREEN)
+                binding.ownedView.setTextColor(Color.GREEN)
             } else {
-                ownedView.setTextColor(Color.GRAY)
+                binding.ownedView.setTextColor(Color.GRAY)
             }
         }
+
         companion object {
-             fun from(parent: ViewGroup): ViewHolder {
+            fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.customer_item_view, parent, false) as LinearLayout
-                return ViewHolder(view)
+                val binding = CustomerItemViewBinding.inflate(layoutInflater, parent, false)
+//                val view = layoutInflater.inflate(R.layout.customer_item_view, parent, false) as LinearLayout
+                return ViewHolder(binding)
             }
         }
     }
-
 
 
 }
 
-class CustomerDiffCallback : DiffUtil.ItemCallback<Customer>(){
+class CustomerDiffCallback : DiffUtil.ItemCallback<Customer>() {
     override fun areItemsTheSame(oldItem: Customer, newItem: Customer): Boolean {
         return oldItem.id == newItem.id
     }
